2697b93920e571eb54e8831bd472995d8f8eea1f
Juliano Gomes <julianogomes23@hotmail.com>
2023-02-12 03:45:35 -0300

    Update ex003.js

--- aula 06/ex003.js
+++ aula 06/ex003.js
@@ -7,3 +7,17 @@ window.alert('A soma dos valores é ' + soma)
 // O + pode ser usar para concateenação e para soma
 // (string + string) concatenação
 // (soma + soma) soma
+// Para conerver para string podemos usar String(n) e n.string()
+// Para converter para number podemos usar  parseInt (Numero inteiro) ou parseFloat(Numero com virgula).
+
+var nome = 'Juliano'
+var idade = 18
+var nota = 7.0
+
+window.alert(
+  'O Aluno ' + nome + ' de ' + idade + ' anos tirou uma nota de ' + nota
+)
+// Podemos realizar a concateenação com o sinal de +, porém, existe uma forma mais semantica de realizar a mesma operação acima com a interpolação.
+
+window.alert(`O aluno ${nome} de ${idade} anos tirou nota ${nota}`)
+// Podemos utilizar ${} para realizar a função do + na concatenação porem de fomra mais simples.
