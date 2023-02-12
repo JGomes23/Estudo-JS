071512863176c947a9ecefda57a9b1444c76373a
Juliano Gomes <julianogomes23@hotmail.com>
2023-02-12 02:28:49 -0300

    Estudo de JS

---
+++ aula 04/ex001.js
@@ -0,0 +1,3 @@
+window.alert('Minha Primeira Mensagem!')
+window.confirm('Você esta gostando d JavaScript ?')
+window.prompt('Qual é o seu nome ?')
---
+++ aula 06/ex002.js
@@ -0,0 +1,4 @@
+window.confirm('Você esta gostando de JavaScript ?')
+var nome = window.prompt('Qual é o seu nome ?')
+
+window.alert('Seja bem Vindo, ' + nome)
