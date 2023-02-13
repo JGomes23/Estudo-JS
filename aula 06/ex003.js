fada0eed17f60fcbc970fb2b5bb02dfb76301041
Juliano Gomes <julianogomes23@hotmail.com>
2023-02-13 02:02:41 -0300

    update ex 003js

--- aula 06/ex003.js
+++ aula 06/ex003.js
@@ -1,12 +1,12 @@
 var n1 = Number.parseInt(window.prompt('Digite um número.'))
 var n2 = Number.parseInt(window.prompt('Digite outro número.'))
-// Pelo falto de ser um prompt o n1 e o n2 estão recebendo uma string, sendo necessario adicionana um parseInt (Numero inteiro) ou parseFloat(Numero com virgula).
+// Pelo falto de ser um prompt o n1 e o n2 estão recebendo uma string, sendo necessario adicionana um parseInt (Numero inteiro) ou parseFloat(Numero com virgula) para adicionar números.
 var soma = n1 + n2
 window.alert('A soma dos valores é ' + soma)
 
 // O + pode ser usar para concateenação e para soma
 // (string + string) concatenação
-// (soma + soma) soma
+// (numero + numero) soma
 // Para conerver para string podemos usar String(n) e n.string()
 // Para converter para number podemos usar  parseInt (Numero inteiro) ou parseFloat(Numero com virgula).
 
