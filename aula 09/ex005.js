db776b465f90a2d2333110f01b576cb89a0fb2a5
Juliano Gomes <julianogomes23@hotmail.com>
2023-02-14 03:36:12 -0300

    DOM

---
+++ aula 09/ex005.js
@@ -0,0 +1,4 @@
+var p1 = window.document.getElementsByTagName('p')[0]
+//O método getElementsByTagName da interface Document retorna um HTMLCollection de elementos com o nome da tag fornecido.
+document.write(p1.innerText)
+//innerText é uma propriedade que representa o conteúdo textual "renderizado" de um nó e seus descendentes.
