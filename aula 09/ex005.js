14f66aad705f4bbd4dfc872b6e4ba66730109984
Juliano Gomes <julianogomes23@hotmail.com>
2023-02-14 03:44:35 -0300

    Update ex005

--- aula 09/ex005.js
+++ aula 09/ex005.js
@@ -1,4 +1,8 @@
-var p1 = window.document.getElementsByTagName('p')[0]
+var p1 = window.document.getElementsByTagName('p')[1]
 //O método getElementsByTagName da interface Document retorna um HTMLCollection de elementos com o nome da tag fornecido.
-document.write(p1.innerText)
-//innerText é uma propriedade que representa o conteúdo textual "renderizado" de um nó e seus descendentes.
+document.write(`Está escrito assim: ${p1.innerText}<br>`)
+//innerText é uma propriedade que representa o conteúdo textual "renderizado" de um nó e seus descendentes. O inner text pega somente o texto sem a formatação.
+p1.style.color = 'black'
+// foi usado .style.color para mudar a cor do elemento selecionado da variavel p1.
+document.write(`Está escrito assim: ${p1.innerHTML}`)
+//O innerHTML é uma propriedade do Element que define ou retorna o conteúdo HTML de um elemento. O innerHTML pega a parte do codigo HTML selecionado ja com a formatação.
