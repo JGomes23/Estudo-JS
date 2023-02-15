95f3d8ac6a88f861e9eaf5a229027291f408c0e6
Juliano Gomes <julianogomes23@hotmail.com>
2023-02-14 22:32:43 -0300

    Eventos e Funções

--- aula 10/ex006.js
+++ aula 10/ex006.js
@@ -1,4 +1,12 @@
+var a = window.document.getElementById('area')
+
 function clicar() {
-  var a = window.document.getElementById('area')
   a.innerText = 'Clicou!'
+}
+// A function  é um conjunto de codigos que vão ser executados quando o Evento ocorrer.
+function entrar() {
+  a.innerText = 'Entrou!'
+}
+function sair() {
+  a.innerText = 'Saiu!'
 }
