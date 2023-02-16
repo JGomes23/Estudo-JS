0ac290b0fb7586bd68cd9d223974747435c6113c
Juliano Gomes <julianogomes23@hotmail.com>
2023-02-16 02:29:58 -0300

    Create ex008.js

---
+++ aula 11/ex008.js
@@ -0,0 +1,10 @@
+var vel = 400.5
+console.log(`A velocidade do seu carro é de ${vel}Km/h por hora.`)
+
+if (vel > 40) {
+  console.log('Você ultrapassou o limite de Velocidade, MULTADO')
+  //A condicional if é uma estrutura condicional que executa a afirmação, dentro do bloco, se determinada condição for verdadeira.
+} else {
+  console.log('Dirija sempre usando cinto de segurança.')
+  //Se for falsa, executa as afirmações dentro de else.
+}
