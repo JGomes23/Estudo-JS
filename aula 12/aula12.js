03cb6a5aa9c6bfb52adf277b50923fd8af40852e
Juliano Gomes <julianogomes23@hotmail.com>
2023-02-16 23:37:04 -0300

    Update Operadores aninhados

--- aula 12/aula12.js
+++ aula 12/aula12.js
@@ -1,11 +1,12 @@
-var idade = 18
+var idade = 63
 if (idade < 16) {
   console.log('Não Vota')
 } else {
-  if (idade >= 16 && idade < 18) {
+  if (idade < 18 || idade >= 65) {
     console.log('Voto opcional')
   }
-  if (idade >= 18) {
+
+  if (idade >= 18 && idade < 65) {
     console.log('Voto Obrigatorio')
   }
 }
