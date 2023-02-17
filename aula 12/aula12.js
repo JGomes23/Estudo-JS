94c40804ffb2724e2aa476ecb5241fe27fa7ede7
Juliano Gomes <julianogomes23@hotmail.com>
2023-02-16 23:42:13 -0300

    Update aula12.js

--- aula 12/aula12.js
+++ aula 12/aula12.js
@@ -1,12 +1,13 @@
 var idade = 63
+console.log(`Você tem ${idade} anos`)
 if (idade < 16) {
-  console.log('Não Vota')
+  console.log('E você Não Vota')
 } else {
   if (idade < 18 || idade >= 65) {
-    console.log('Voto opcional')
+    console.log('E o seu Voto é opcional')
   }
 
   if (idade >= 18 && idade < 65) {
-    console.log('Voto Obrigatorio')
+    console.log('E o seu Voto é Obrigatorio')
   }
 }
